3c794fdb1d7f8652c7404ece854c04bb877fa3f9
magisystem0408 <masato.matsudo@gmail.com>
2021-12-15 16:57:15 +0900

    add nonce

--- utils.py
+++ utils.py
@@ -16,5 +16,28 @@ def sorted_dict_by_key(unsorted_dict):
     )
 
 
+
+def pprint(chains):
+    for i, chain in enumerate(chains):
+        print(f'{"=" * 25} Chain {i} {"=" * 25}')
+        '''　↑と表示する
+        ========================= Chain 1 =========================
+        '''
+        for k, v in chain.items():
+
+            if k == "transactions":
+                print(k)
+                for d in v:
+                    print(f'{"-" * 40}')
+                    for kk,vv in d.items():
+                        print(f"{kk:30}{vv}")
+            else:
+                print(f'{k:15}{v}')
+
+        print(f'{"*" * 25}')
+
+
+
+
 # ブロックごとハッシュ化する
 print(hashlib.sha256(str(block).encode()).hexdigest())
